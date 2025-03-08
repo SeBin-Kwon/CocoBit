c6793f703043fd8333dd11fd2c064339a1491468
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-09 02:16:30 +0900

    Feat: SearchViewModel 추가

---
+++ CocoBit/Present/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  SearchViewModel.swift
+//  CocoBit
+//
+//  Created by Sebin Kwon on 3/9/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class SearchViewModel: BaseViewModel {
+    
+    var disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
