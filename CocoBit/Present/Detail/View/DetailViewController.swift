76c635366486ea8840cc4546ef71007f6ac8530b
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-09 16:33:13 +0900

    Feat: DetailViewController 추가

---
+++ CocoBit/Present/Detail/View/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  CocoBit
+//
+//  Created by Sebin Kwon on 3/9/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+class DetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "detail"
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
+
+// MARK: View Layout
+extension SearchResultViewController {
+    private func configureHierarchy() {
+        
+    }
+    
+    private func configureLayout() {
+        
+    }
+}
