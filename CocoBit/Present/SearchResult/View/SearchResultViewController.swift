814c557c899955dd0e8879f7ed64cff4551e0caa
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-09 16:32:10 +0900

    Feat: SearchResultViewController 생성

---
+++ CocoBit/Present/SearchResult/View/SearchResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SearchResultViewController.swift
+//  CocoBit
+//
+//  Created by Sebin Kwon on 3/9/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+class SearchResultViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "result"
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
+
+// MARK: View Layout
+extension SearchResultViewController {
+    private func configureHierarchy() {
+        
+    }
+    
+    private func configureLayout() {
+        
+    }
+}
