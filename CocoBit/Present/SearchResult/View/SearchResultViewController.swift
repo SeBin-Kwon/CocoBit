2907fc6ba641ae45615c444ea206abde819b4b8f
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-09 23:26:56 +0900

    Feat: SearchResultView CollectionView 구성

--- CocoBit/Present/SearchResult/View/SearchResultViewController.swift
+++ CocoBit/Present/SearchResult/View/SearchResultViewController.swift
@@ -11,36 +11,63 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-final class SearchTextFieldView: BaseView {
-    let textField = {
-        let textfield = UITextField()
-        textfield.backgroundColor = .red
-        return textfield
-    }()
-    
-    override func configureHierarchy() {
-        addSubview(textField)
-    }
-    
-    override func configureLayout() {
-        textField.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview()
-        }
-    }
-}
-
-
-
 final class SearchResultViewController: BaseViewController {
     
-    let searchText: String
-    let searchTextField = {
+    private let searchText: String
+    private let searchTextField = {
         let textfield = UITextField()
         textfield.textColor = .cocoBitBlack
         textfield.frame = CGRect(x: 0, y: 0, width: 300, height: 30)
         return textfield
     }()
     
+//    private let segmentControl: UISegmentedControl = {
+//            let segment = UISegmentedControl()
+//            segment.insertSegment(withTitle: "홈", at: 0, animated: true)
+//            segment.insertSegment(withTitle: "전체메뉴", at: 1, animated: true)
+//            segment.insertSegment(withTitle: "최근리뷰", at: 2, animated: true)
+//            segment.selectedSegmentIndex = 0   // 첫번째 segmented control의 인덱스 값
+//        
+//        segment.setTitleTextAttributes([
+//            NSAttributedString.Key.foregroundColor: UIColor.cocoBitBlack,
+//            NSAttributedString.Key.font: UIFont.setFont(.subTitle)
+//                ], for: .normal)
+//                segment.setTitleTextAttributes([
+//                    NSAttributedString.Key.foregroundColor: UIColor.cocoBitGray,
+//                    NSAttributedString.Key.font: UIFont.setFont(.subTitle)
+//                ], for: .selected)
+//        
+//        segment.selectedSegmentTintColor = .clear
+//        segment.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+//                segment.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+//            return segment
+//        }()
+//    
+//    private let underLineView: UIView = {
+//            let view = UIView()
+//        view.backgroundColor = .cocoBitBlack
+//            return view
+//        }()
+    
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    
+    private let dataSource = RxCollectionViewSectionedReloadDataSource<TrendingSectionModel> { dataSource, collectionView, indexPath, item in
+
+        switch item {
+        case .coin(let item):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.identifier, for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
+            cell.configureData(item)
+            cell.backgroundColor = .lightGray
+            return cell
+            
+        case .nft(let item):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NFTCollectionViewCell.identifier, for: indexPath) as? NFTCollectionViewCell else { return UICollectionViewCell() }
+            cell.configureData(item)
+            return cell
+        }
+        
+    }
+                                                                                              
     init(searchText: String) {
         self.searchText = searchText
         super.init()
@@ -51,10 +78,47 @@ final class SearchResultViewController: BaseViewController {
         configureNavigationBar()
         configureHierarchy()
         configureLayout()
+        bind()
+        
+        collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
+        collectionView.register(NFTCollectionViewCell.self, forCellWithReuseIdentifier: NFTCollectionViewCell.identifier)
     }
     
-    
+    private func bind() {
+        var coinList: [SectionItem] = [SectionItem.coin(model: CoinItem(score: "2", symbol: "3", name: "4", change: "5", changeColor: .down, image: "6")),
+                                       SectionItem.coin(model: CoinItem(score: "2", symbol: "3", name: "4", change: "5", changeColor: .down, image: "6")),
+                                       SectionItem.coin(model: CoinItem(score: "2", symbol: "3", name: "4", change: "5", changeColor: .down, image: "6")),
+                                       SectionItem.coin(model: CoinItem(score: "2", symbol: "3", name: "4", change: "5", changeColor: .down, image: "6"))]
+        
+        
+        let sections = BehaviorSubject<[TrendingSectionModel]>(value: [.coinSection(header: ("인기 검색어", "ss"), data: coinList)])
+        
+        sections
+            .bind(to: collectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
 
+}
+
+// MARK: CollectionView Layout
+extension SearchResultViewController {
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/7))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0) // 아이템 간 간격
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.55))
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+//        section.interGroupSpacing = 20 // 그룹간 간격
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
 }
 
 
@@ -68,10 +132,12 @@ extension SearchResultViewController {
     }
     
     private func configureHierarchy() {
-        
+        view.addSubview(collectionView)
     }
     
     private func configureLayout() {
-        
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
