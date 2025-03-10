d335ed8ca80eed944030518ccea4cd74b65ebd19
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-10 20:39:17 +0900

    Feat: DetailData 모델 정의

---
+++ CocoBit/Model/DetailData.swift
@@ -0,0 +1,48 @@
+//
+//  DetailData.swift
+//  CocoBit
+//
+//  Created by Sebin Kwon on 3/10/25.
+//
+
+import Foundation
+
+struct DetailData: Decodable {
+    let image: String
+    let symbol: String
+    let crrentPrice: Double
+    let change24h: Double
+    let lastUpdated: String
+    let high24h: Double
+    let low24h: Double
+    let ath: Double
+    let athDate: String
+    let atl: Double
+    let atlDate: String
+    let marketCap: Double
+    let fullyValuation: Double
+    let totalVolum: Double
+    let sparklineIn7d: Sparkline
+    
+    enum CodingKeys: String, CodingKey {
+        case image
+        case symbol
+        case crrentPrice = "current_price"
+        case change24h = "price_change_percentage_24h"
+        case lastUpdated = "last_updated"
+        case high24h = "high_24h"
+        case low24h = "low_24h"
+        case ath
+        case athDate = "ath_date"
+        case atl
+        case atlDate = "atl_date"
+        case marketCap = "market_cap"
+        case fullyValuation = "fully_diluted_valuation"
+        case totalVolum = "total_volume"
+        case sparklineIn7d = "sparkline_in_7d"
+    }
+}
+
+struct Sparkline: Decodable {
+    let price: [Double]
+}
