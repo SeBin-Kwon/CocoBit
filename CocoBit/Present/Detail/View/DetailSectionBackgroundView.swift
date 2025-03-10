441cb49604d87f7d8dde50b473f6d6e121d4e884
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-10 19:37:35 +0900

    Feat: DetailSectionBackgroundView 구현

---
+++ CocoBit/Present/Detail/View/DetailSectionBackgroundView.swift
@@ -0,0 +1,37 @@
+//
+//  DetailSectionBackgroundView.swift
+//  CocoBit
+//
+//  Created by Sebin Kwon on 3/10/25.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailSectionBackgroundView: UICollectionReusableView {
+    static var identifier: String {
+        String(describing: self)
+    }
+    
+    private let backgroundView = {
+       let view = UIView()
+        view.backgroundColor = .cocoBitLightGray
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        addSubview(backgroundView)
+        backgroundView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(40)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
