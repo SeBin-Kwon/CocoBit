be8378bceb0786dc96aa6873092e41fe30b9d57e
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-11 15:30:46 +0900

    Feat: 토스트 익스텐션 구현

--- CocoBit/Utilities/Extension/UIViewController+Extension.swift
+++ CocoBit/Utilities/Extension/UIViewController+Extension.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Toast
 
 extension UIViewController {
     func navigate(_ type: NavigationType) {
@@ -14,6 +15,14 @@ extension UIViewController {
         case .pop: self.navigationController?.popViewController(animated: true)
         case .present(let vc): self.present(vc, animated: true)
         case .dismiss: self.dismiss(animated: true)
+        }
+    }
+    
+    func toast(_ value: Bool, _ name: String) {
+        if value {
+            view.makeToast("\(name)(이/가) 즐겨찾기되었습니다.")
+        } else {
+            view.makeToast("\(name)(이/가) 즐겨찾기에서 제거되었습니다.")
         }
     }
 }
