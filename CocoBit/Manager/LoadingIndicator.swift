7fa20b554d82028d93826419dac6a51100643ee0
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-11 20:34:09 +0900

    Feat: LodingIndicator 구현

---
+++ CocoBit/Manager/LoadingIndicator.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingIndicator.swift
+//  CocoBit
+//
+//  Created by Sebin Kwon on 3/11/25.
+//
+
+import UIKit
+
+class LoadingIndicator {
+    static func showLoading() {
+        DispatchQueue.main.async {
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let window = windowScene.windows.first else { return }
+
+            let loadingIndicatorView: UIActivityIndicatorView
+            if let existedView = window.subviews.first(where: { $0 is UIActivityIndicatorView } ) as? UIActivityIndicatorView {
+                loadingIndicatorView = existedView
+            } else {
+                loadingIndicatorView = UIActivityIndicatorView(style: .large)
+                loadingIndicatorView.frame = window.frame
+                loadingIndicatorView.color = .cocoBitGray
+                window.addSubview(loadingIndicatorView)
+            }
+
+            loadingIndicatorView.startAnimating()
+        }
+    }
+    
+    static func hideLoading() {
+        DispatchQueue.main.async {
+            DispatchQueue.main.async {
+                guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                      let window = windowScene.windows.first else { return }
+                    window.subviews.filter({ $0 is UIActivityIndicatorView }).forEach { $0.removeFromSuperview() }
+                }
+        }
+    }
+}
